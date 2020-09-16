1c1755c4eadd7dfc30aceae550c20c8994617d43
Dane Bulat <dane.bulat@gmail.com>
2020-09-16 21:05:22 +0900

    demo 2 code refactored into Demo2 class

--- lib/demos.hpp
+++ lib/demos.hpp
@@ -222,3 +222,186 @@ public:
 		ImGui::ShowMetricsWindow(&m_open);
 	}
 };
+
+/*----------------------------------------------------------------------
+  Demo2 class
+  ----------------------------------------------------------------------*/
+class Demo2 {
+private:
+	Point m_point;
+	Rect  m_rect;
+
+	int m_combo_index;
+	int m_list_index;
+
+	std::array<int, 4> 	 	 m_arr1;
+	std::vector<int>   		 m_arr2;
+	std::vector<std::string> m_combo_vec;
+	std::vector<std::string> m_list_vec;
+	std::vector<char>		 m_text_input_buffer;
+
+	std::array<char, 100> m_text_arr;
+	const char m_replacement_char = 'D';
+
+private:
+
+	// free function callback for InputText widget
+	// makes every inputted character an 'A'
+	static int callback(ImGuiTextEditCallbackData* data) {
+		data->EventChar = 'A';
+		return 0;
+	}
+
+	// callback examples
+	void run_callback_examples() {
+
+		// passing a free callback function to InputText
+		ImGui::InputText(
+			"Text",										// widget label
+			m_text_arr.data(),							// input text buffer
+			m_text_arr.size(),							// input text buffer size
+			ImGuiInputTextFlags_CallbackCharFilter,  	// char filter flag
+			callback);									// callback function
+
+		// you can also pass a stateless lambda instead of a free function
+		ImGui::InputText(
+			"Text2",									// widget label
+			m_text_arr.data(),							// input text buffer
+			m_text_arr.size(),							// input text buffer size
+			ImGuiInputTextFlags_CallbackCharFilter,	   	// char filter flag
+			[](ImGuiTextEditCallbackData* data) {	   	// callback lambda
+				data->EventChar = 'A';
+				return 0;
+			});
+
+		// suppose we want to use a char variable instead of a hard-coded 'A'
+		ImGui::InputText(
+			"Text3",									// widget label
+			m_text_arr.data(),							// input text buffer
+			m_text_arr.size(),							// input text buffer size
+			ImGuiInputTextFlags_CallbackCharFilter,		// char filter flag
+			[](ImGuiInputTextCallbackData* data) {		// callback lambda
+				data->EventChar = *static_cast<const char*>(data->UserData);
+				return 0;
+			},
+			const_cast<char*>(&m_replacement_char));	// attached user data
+
+		// passing a lambda with state with custom ImGui::InputTextCool function
+		char replacement_char = 'R';
+
+		if (ImGui::InputTextCool(
+			"Text4",												// widget label
+			m_text_arr.data(),										// input text buffer
+			m_text_arr.size(),										// input text buffer size
+			ImGuiInputTextFlags_CallbackCharFilter,					// char filter flag
+			[&replacement_char](ImGuiTextEditCallbackData* data) {  // lambda with state
+				data->EventChar = replacement_char;
+				std::cout << "Replaced with: " << replacement_char << "\n";
+				return 0;
+			})) {
+			/* Do stuff when text changes */
+		}
+
+		/* --------------------------------------------------------------------------------------
+		Callback example 3: Generalising the approach
+		(requires boost function_traits / template programming)
+		// TODO: https://eliasdaler.github.io/using-imgui-with-sfml-pt2/
+		--------------------------------------------------------------------------------------- */
+	}
+
+public:
+	Demo2() : m_combo_index(0)
+			, m_list_index(0) {
+
+		m_arr1 = { 0 };
+		m_text_arr = { '\0' };
+		m_arr2.resize(4, 0);
+
+		m_combo_vec = { "Camera Animation", "Circle Tween", "Easing Functions" };
+		m_list_vec = { "Camera Animation", "Circle Tween", "Easing Functions" };
+		m_text_input_buffer = {  't', 'e', 's', 't' };
+	}
+
+	void run(sf::RenderWindow& window) {
+		ImGui::Begin("Sample Window");
+
+		// using "##" to define unique labels but with same display string
+		if (ImGui::Button("Same Label")) {
+			std::cout << "Button 1 pushed" << std::endl;
+		}
+
+		if (ImGui::Button("Same Label##Second")) {
+			std::cout << "Button 2 pushed" << std::endl;
+		}
+
+		// Using an array to define input boxes
+		std::array<int, 4> arr = { 0 };
+
+		for (int i = 0; i < arr.size(); ++i) {
+			ImGui::PushID(i);
+			ImGui::InputInt("##", &arr[i]);
+			ImGui::PopID();
+		}
+		ImGui::End();
+
+		// create another window
+		ImGui::Begin("Sample Window #2");
+
+		if (ImGui::Button("Button")) {
+			std::cout << "button clicked" << std::endl;
+		}
+		ImGui::End();
+
+		// append to an existing window
+		ImGui::Begin("Sample Window");
+
+		if (ImGui::Button("Appended Button")) {
+			std::cout << "appended button clicked" << std::endl;
+		}
+
+		// using structs with POD - interpreted as array of floats
+		if (ImGui::InputFloat2("Point", &m_point.x)) {
+			std::cout << "point changed" << std::endl;
+		}
+
+		// using structs with POD - interpreted as array of floats
+		if (ImGui::InputFloat4("Rect", &m_rect.x)) {
+			std::cout << "rect changed" << std::endl;
+		}
+
+		// call custom ImGui::InputRect function
+		if (ImGui::InputRect("Rect Custom", &m_rect)) {
+			std::cout << "rect changed #2" << std::endl;
+		}
+
+		// call std::array::data() and std::vector::data() to pass the raw
+		// pointer to the internal array to ImGui
+		ImGui::InputInt4("STL Array", m_arr1.data());
+		ImGui::InputInt4("STL Vector", m_arr2.data());
+
+		// Use custom Combo and Listbox function to pass std::vector to
+		// ComboBox and ListBox
+		ImGui::BeginGroup();
+		ImGui::PushItemWidth(140.f);
+		if (ImGui::Combo("Sample Combo", &m_combo_index, m_combo_vec)) {
+			std::cout << "> combo change: " << m_combo_vec[m_combo_index] << "\n";
+		}
+		ImGui::SameLine();
+
+		ImGui::PushItemWidth(140.f);
+		if (ImGui::ListBox("Sample List", &m_list_index, m_list_vec)) {
+			std::cout << "> list change: " << m_list_vec[m_list_index] << "\n";
+		}
+		ImGui::SameLine();
+		ImGui::EndGroup();
+
+		// InputText and std::string
+		// Can pass vector<char> or array<char> to InputText and initialise string to vector.at()
+		ImGui::InputText("Sample Input",
+			m_text_input_buffer.data(), m_text_input_buffer.size() + 1);
+
+		run_callback_examples();
+
+		ImGui::End();
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -11,95 +11,6 @@
 
 #include "demos.hpp"
 
-/** Demo 2 Data **/
-struct d2 {
-	static Point p;
-	static Rect r;
-	static std::array<int, 4> arr1;
-	static std::vector<int> arr2;
-	static std::vector<std::string> comboVec;
-	static std::vector<std::string> listVec;
-	static int comboIndex;
-	static int listIndex;
-	static std::vector<char> textInputBuffer;
-};
-
-Point d2::p;
-Rect  d2::r;
-std::array<int, 4> d2::arr1 = { 0 };
-std::vector<int> d2::arr2(4, 0);
-std::vector<std::string> d2::comboVec = { "Camera Animation", "Circle Tween", "Easing Functions" };
-int d2::comboIndex = 0;
-std::vector<std::string> d2::listVec = { "Camera Animation", "Circle Tween", "Easing Functions" };
-int d2::listIndex = 0;
-std::vector<char> d2::textInputBuffer = {'t', 'e', 's', 't'};
-
-/* --------------------------------------------------------------------------------
-  Callback example 1: Change all input to A
-  -------------------------------------------------------------------------------- */
-
-// 1. first you define a free function
-int callback(ImGuiTextEditCallbackData* data) {
-	data->EventChar = 'A'; // makes every character fed to InputText an 'A'
-	return 0;
-}
-
-std::array<char, 100> textArr = {' '};
-
-const char replacementChar = 'Z';
-
-void callbackExample() {
-
-	// 2a. Then pass it to InputText
-	ImGui::InputText("Text", textArr.data(), textArr.size(),
-		ImGuiInputTextFlags_CallbackCharFilter, callback); // specify we're using a callbackCharFilter
-
-	// 2b. You can also use stateless lambdas which can be used just as free functions
-	ImGui::InputText("Text2", textArr.data(), textArr.size(),
-		ImGuiInputTextFlags_CallbackCharFilter,
-		[](ImGuiTextEditCallbackData* data) {
-			data->EventChar = 'A';
-			return 0;
-		}
-	);
-
-	// 3. Suppose we want to use a char variable instead of a hard-code replacement
-	//    We can store the pointer to a char in ImGuiEditCallbackData::UserData
-	ImGui::InputText("Text3", textArr.data(), textArr.size(),
-		ImGuiInputTextFlags_CallbackCharFilter,
-		[](ImGuiTextEditCallbackData* data) {
-			data->EventChar = *static_cast<const char *>(data->UserData);
-			return 0;
-		},
-		const_cast<char*>(&replacementChar)); // cast away constness (store in data->UserData)
-}
-
-/* --------------------------------------------------------------------------------------
-  Callback example 2: Defining a wrapper function that lets us pass lambdas with state
-  --------------------------------------------------------------------------------------- */
-
-// Now we can pass a lambda with state like this:
-void callbackExample02(const char replacementCh) {
-
-	if (ImGui::InputTextCool("Text4", textArr.data(), textArr.size(),
-		ImGuiInputTextFlags_CallbackCharFilter,
-		[&replacementCh](ImGuiTextEditCallbackData* data) {
-			data->EventChar = replacementCh;
-			std::cout << "Replaced with: " << replacementCh << "\n";
-			return 0;
-		})) {
-			/* Do stuff when text changes */
-	}
-}
-
-/* --------------------------------------------------------------------------------------
-  Callback example 3: Generalising the approach
-  (requires boost function_traits / template programming)
-  // TODO: https://eliasdaler.github.io/using-imgui-with-sfml-pt2/
-  --------------------------------------------------------------------------------------- */
-
-/** End Demo 2 Data **/
-
 /** Demo 3 **/
 struct d3 {
 	static bool show_window;
@@ -118,7 +29,6 @@ bool d3::my_tool_active = true;
 float d3::my_color[4] = {0.f};
 /** End Demo 3 **/
 
-void runDemo2(sf::RenderWindow&);
 void runDemo3(sf::RenderWindow&);
 
 int main()
@@ -135,11 +45,12 @@ int main()
 	sf::Clock deltaClock;
 
 	Demo1 demo1;
+	Demo2 demo2;
 
 	window.setTitle(demo1.getWindowTitle());
     window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
 
-	sf::Uint8 current_demo = 1;
+	sf::Uint8 current_demo = 2;
 
 	while (window.isOpen()) {
         sf::Event event;
@@ -159,7 +70,7 @@ int main()
 
 		switch (current_demo) {
 			case 1:  demo1.run(window); break;
-			case 2:  runDemo2(window); break;
+			case 2:  demo2.run(window); break;
 			case 3:  runDemo3(window); break;
 			default: demo1.run(window); break;
 		}
@@ -176,89 +87,6 @@ int main()
     ImGui::SFML::Shutdown();
 
 	return 0;
-}
-
-void runDemo2(sf::RenderWindow& window) {
-
-	ImGui::Begin("Sample Window");
-
-	// Using "##" to define unique labels but with same display string
-	if (ImGui::Button("Same Label")) {
-		std::cout << "Button 1 pushed" << std::endl;
-	}
-	if (ImGui::Button("Same Label##Second")) {
-		std::cout << "Button 2 pushed" << std::endl;
-	}
-
-	// Using an array to define input boxes
-	std::array<int, 4> arr = { 0 };
-
-	for (int i = 0; i < arr.size(); ++i) {
-		ImGui::PushID(i);
-		ImGui::InputInt("##", &arr[i]);
-		ImGui::PopID();
-	}
-	ImGui::End();
-
-	// Create another window
-	ImGui::Begin("Sample Window #2");
-
-	if (ImGui::Button("Button")) {
-		std::cout << "button clicked" << std::endl;
-	}
-	ImGui::End();
-
-	// Append to existing window
-	ImGui::Begin("Sample Window");
-
-	if (ImGui::Button("Appended Button")) {
-		std::cout << "appended button clicked" << std::endl;
-	}
-
-	// Using structs with POD - interpreted as array of floats
-	if(ImGui::InputFloat2("Point", &d2::p.x)) {
-		std::cout << "point changed" << std::endl;
-	}
-
-	// Using structs with POD - interpreted as array of floats
-	if(ImGui::InputFloat4("Rect", &d2::r.x)) {
-		std::cout << "rect changed" << std::endl;
-	}
-
-	if (ImGui::InputRect("Rect Custom", &d2::r)) {
-		std::cout << "rect changed #2" << std::endl;
-	}
-
-	// call std::array::data() and std::vector::data() to pass the raw
-	// pointer to the internal array to ImGui
-	//ImGui::PushItemWidth(128.f);
-	ImGui::InputInt4("STL Array", d2::arr1.data());
-	ImGui::InputInt4("STL Vector", d2::arr2.data());
-
-	// Use redefined callback to pass std::vector to ComboBox and ListBox
-	ImGui::BeginGroup();
-	ImGui::PushItemWidth(140.f);
-	if (ImGui::Combo("Sample Combo", &d2::comboIndex, d2::comboVec)) {
-		std::cout << "> combo change: " << d2::comboVec[d2::comboIndex] << "\n"; }
-	ImGui::SameLine();
-
-	ImGui::PushItemWidth(140.f);
-	if (ImGui::ListBox("Sample List", &d2::listIndex, d2::listVec)) {
-		std::cout << "> list change: " << d2::listVec[d2::listIndex] << "\n"; }
-	ImGui::SameLine();
-	ImGui::EndGroup();
-
-	// InputText and std::string
-	// Can pass vector<char> or array<char> to InputText and initialise string to vector.at()
-	ImGui::InputText("Sample Input", d2::textInputBuffer.data(), d2::textInputBuffer.size() + 1);
-
-	// Callback example
-	callbackExample();
-
-	// Callback example #2
-	callbackExample02('R');
-
-	ImGui::End();
 }
 
 void runDemo3(sf::RenderWindow& window) {
